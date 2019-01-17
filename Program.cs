1c8e44c1ce7148665e746dfa314c2dfd1fa9d099
Ravinderfb21 <46794832+Ravinderfb21@users.noreply.github.com>
2019-01-17 16:57:29 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consoleco696464
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // var dogs = new List<String>();
+            //dogs.Add("Bichon");
+          // dogs.Add("Podle");
+
+            /* foreach (var dog in dogs)
+             {
+                 Console.WriteLine(dog + " ");
+             }*/
+
+            // Console.WriteLine(dogs[1]);
+            //Console.ReadLine();
+
+            //NOW CREATE AN ARRAY!!!
+
+            int[] a1 = new int[] { 1, 3, 5, 7, 9, 11 };
+
+            foreach (int i in a1)
+            {
+                Console.WriteLine( i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
